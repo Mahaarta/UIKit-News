59560250cee1b31b65b886afb8f6848525eb2964
Minata P Mahaarta <minataputra@gmail.com>
2024-12-05 12:52:10 +0700

    Initial Commit

---
+++ news ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  news ios
+//
+//  Created by Minata on 05/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
